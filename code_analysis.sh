a3875740246c9088ac3a63129b944f844d2b99c9
Benjamin Auffarth <Benjamin.Auffarth@myjar.com>
2016-09-15 01:41:14 +0100

    each file only once

--- code_analysis.sh
+++ code_analysis.sh
@@ -15,25 +15,25 @@ do
     filename="${TMPDIR}/${user}.stats"
     rm -f "${filename}"
     commits=$(git log --since="$(date -d '7 days ago' '+%Y-%m-%d')" --author="${user}" --format='%H' | head -n 10)
-    for commit in ${commits}
+    files=$(for commit in ${commits}
     do
-        files=$(git diff-tree --no-commit-id --name-only -r "$commit" | grep 'py$' | cat)
-        for file in $files
-        do
-            if [ -f "$file" ]
+        git diff-tree --no-commit-id --name-only -r "$commit" | grep 'py$' | cat
+    done | sort -u)
+    for file in $files
+    do
+        if [ -f "$file" ]
+        then
+            ext="${file##*.}"
+            if [[ "$ext" = "py" ]]
             then
-                    ext="${file##*.}"
-                    if [[ "$ext" = "py" ]]
-                    then
-                        error_contrib=$(responsible "$file" | grep "$user" | tail -n 1 | awk '{ print $1 }' )
-                        lines=$(wc -l < "$file")
-                        errors=$(flake8 "$file" | wc -l)
-                        ugly=$(echo "$errors * $error_contrib" | bc -l)
-                        echo "$ugly,$lines"
-                    fi
+                error_contrib=$(responsible "$file" | grep "$user" | tail -n 1 | awk '{ print $1 }' )
+                lines=$(wc -l < "$file")
+                errors=$(flake8 "$file" | wc -l)
+                ugly=$(echo "$errors * $error_contrib" | bc -l)
+                echo "$ugly,$lines"
             fi
-        done >> "$filename"
-    done
+        fi
+    done >> "$filename"
 done
 
 # output
