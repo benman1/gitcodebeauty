4497e2fd41a9782292abfa86e7ee35f2f67c6d75
Benjamin Auffarth <Benjamin.Auffarth@myjar.com>
2016-09-15 01:37:12 +0100

    change metric to take into account relative contribution to files

--- code_analysis.sh
+++ code_analysis.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
-TMPDIR="/tmp/gitybeauty"
+#!/bin/bash
+TMPDIR="/tmp/gitbeauty"
 users=$(git shortlog -ns --since="$(date -d '7 days ago' '+%Y-%m-%d')" | awk '{print $2}' | sort -u)
+
+function responsible {
+    sum=$(wc -l < "$file")
+    git blame -w --line-porcelain "$1" | grep -a "^author " | sort -f | uniq -c | sort -n | awk -v sum="$sum" '{ print $1/sum,$3 }'
+}
 
 mkdir -p ${TMPDIR}
 rm -f ${TMPDIR}/*
-#for user in "${users[@]}"
 for user in ${users}
 do
     filename="${TMPDIR}/${user}.stats"
-    #echo "filename: ${filename}"
-    rm -f ${filename}
+    rm -f "${filename}"
     commits=$(git log --since="$(date -d '7 days ago' '+%Y-%m-%d')" --author="${user}" --format='%H' | head -n 10)
     for commit in ${commits}
     do
-        for file in $(git diff-tree --no-commit-id --name-only -r ${commit} | grep grep '.py$' | cat)
-        # 'i*pyn*b*$'
+        files=$(git diff-tree --no-commit-id --name-only -r "$commit" | grep 'py$' | cat)
+        for file in $files
         do
             if [ -f "$file" ]
             then
-                    #echo "${file}"
                     ext="${file##*.}"
-                    if [[ "$ext" != "ipynb" ]]
+                    if [[ "$ext" = "py" ]]
                     then
-                            #echo "$(jupyter nbconvert ${file} --stdout --to script | flake8 - --ignore=W391 | wc -l),$(cat ${file} | wc -l)"
-                            echo $(flake8 ${file} | wc -l),$(cat ${file} | wc -l)
+                        error_contrib=$(responsible "$file" | grep "$user" | tail -n 1 | awk '{ print $1 }' )
+                        lines=$(wc -l < "$file")
+                        errors=$(flake8 "$file" | wc -l)
+                        ugly=$(echo "$errors * $error_contrib" | bc -l)
+                        echo "$ugly,$lines"
                     fi
             fi
-        done >> ${filename}
+        done >> "$filename"
     done
 done
 
 # output
-{ echo "User,prettiness,lines python";
-for file in $(ls -1 ${TMPDIR}/*)
+{ echo "user,prettiness,lines python";
+for file in ${TMPDIR}/*
 do
    if [ -s "$file" ]
        then
-               ugliness=$(awk -F, '{ ugly+=$1;lines+=$2} END {print 1-ugly/lines","lines}' ${file})
+               ugliness=$(awk -F, '{ ugly+=$1;lines+=$2} END {print 1-ugly/lines","lines}' "$file")
                    user=$(basename ${file%.*})
            echo "${user},${ugliness}"
        fi
