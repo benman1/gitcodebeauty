825f2bf5dabb6131060fc61c0a9b62ee244e37ae
Benjamin Auffarth <Benjamin.Auffarth@myjar.com>
2016-09-16 10:01:27 +0100

    cleanup; import order

--- code_analysis.sh
+++ code_analysis.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-#!/bin/bash
 TMPDIR="/tmp/gitbeauty"
-users=$(git shortlog HEAD -ns --since="$(date -d '7 days ago' '+%Y-%m-%d')" | awk '{print $2}' | sort -u)
+VALCMD="flake8 --import-order-style=google"
+
+gnudate() {
+    if hash gdate 2>/dev/null; then
+        gdate "$@"
+    else
+        date "$@"
+    fi
+}
 
 function responsible {
     sum=$(wc -l < "$file")
     git blame -w --line-porcelain "$1" | grep -a "^author " | sort -f | uniq -c | sort -n | awk -v sum="$sum" '{ print $1/sum,$3 }'
 }
 
+users=$(git shortlog HEAD -ns --since="$(gnudate -d '7 days ago' '+%Y-%m-%d')" | awk '{print $2}' | sort -u)
 mkdir -p ${TMPDIR}
 rm -f ${TMPDIR}/*
 for user in ${users}
 do
     filename="${TMPDIR}/${user}.stats"
     rm -f "${filename}"
-    commits=$(git log --since="$(date -d '7 days ago' '+%Y-%m-%d')" --author="${user}" --format='%H' | head -n 10)
+    commits=$(git log --since="$(gnudate -d '7 days ago' '+%Y-%m-%d')" --author="${user}" --format='%H' | head -n 10)
     files=$(for commit in ${commits}
     do
         git diff-tree --no-commit-id --name-only -r "$commit" | grep 'py$' | cat
@@ -28,7 +36,7 @@ do
             then
                 error_contrib=$(responsible "$file" | grep "$user" | tail -n 1 | awk '{ print $1 }' )
                 lines=$(wc -l < "$file")
-                errors=$(flake8 "$file" | wc -l)
+                errors=$($VALCMD "$file" | wc -l)
                 ugly=$(echo "$errors * $error_contrib" | bc -l)
                 echo "$ugly,$lines"
             fi
@@ -42,8 +50,8 @@ for file in ${TMPDIR}/*
 do
    if [ -s "$file" ]
        then
-               ugliness=$(awk -F, '{ ugly+=$1;lines+=$2} END {print 1-ugly/lines","lines}' "$file")
-                   user=$(basename ${file%.*})
+	       ugliness=$(awk -F, '{ ugly+=$1;lines+=$2} END {print 1-ugly/lines","lines}' "$file")
+		   user=$(basename ${file%.*})
            echo "${user},${ugliness}"
        fi
 done; } | csvlook
